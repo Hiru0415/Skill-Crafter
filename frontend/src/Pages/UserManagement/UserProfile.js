cc1e91d3f66e0cf390fa5f5144cabbf0738e3c69
Hiruni Weerasinghe <hiruvishwani15@gmail.com>
2025-05-11 00:35:15 +0530

    edit

--- frontend/src/Pages/UserManagement/UserProfile.js
+++ frontend/src/Pages/UserManagement/UserProfile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaEnvelope, FaPhone, FaTools } from 'react-icons/fa';
-import './UserProfile.css'
+
 import NavBar from '../../Components/NavBar/NavBar';
 export const fetchUserDetails = async (userId) => {
     try {
