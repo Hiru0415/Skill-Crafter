0c58c12dc3d8eca34698a9445aa4bff3c0100dbb
Madara0421D <madaradushyanthi@gmail.com>
2025-05-11 01:45:25 +0530

    updateHandlePost

--- frontend/src/Pages/PostManagement/MyAllPost.js
+++ frontend/src/Pages/PostManagement/MyAllPost.js
@@ -107,7 +107,16 @@ function MyAllPost() {
     navigate(`/updatePost/${postId}`); // Navigate to the UpdatePost page with the post ID
   };
 
-  
+  const handleMyPostsToggle = () => {
+    if (showMyPosts) {
+      // Show all posts
+      setFilteredPosts(posts);
+    } else {
+      // Filter posts by logged-in user ID
+      setFilteredPosts(posts.filter((post) => post.userID === loggedInUserID));
+    }
+    setShowMyPosts(!showMyPosts); // Toggle the state
+  };
 
   const handleLike = async (postId) => {
     const userID = localStorage.getItem('userID');
