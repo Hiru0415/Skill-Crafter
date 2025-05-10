74af11a7c90b61abf02b4cf239934beae8abbdd3
Hiruni Weerasinghe <hiruvishwani15@gmail.com>
2025-05-11 00:54:39 +0530

    Create Exception

--- backend/src/main/java/backend/exception/UserNotFoundAdvice.java
+++ backend/src/main/java/backend/exception/UserNotFoundAdvice.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @ControllerAdvice
 public class UserNotFoundAdvice {
