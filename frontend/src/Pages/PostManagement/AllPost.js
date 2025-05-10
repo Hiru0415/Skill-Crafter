d74a6f1a1e7720e1bc598296f8b0db55eb71e8b0
Madara0421D <madaradushyanthi@gmail.com>
2025-05-11 01:35:56 +0530

    UpdateCreateButton

--- frontend/src/Pages/PostManagement/AllPost.js
+++ frontend/src/Pages/PostManagement/AllPost.js
@@ -337,7 +337,12 @@ function AllPost() {
             <div className="no-posts">
               <h3>No Posts Found</h3>
               <p>Share your thoughts with the community</p>
-              
+              <button 
+                className="create-button"
+                onClick={() => (window.location.href = '/addNewPost')}
+              >
+                Create Post
+              </button>
             </div>
           ) : (
             filteredPosts.map((post) => (
