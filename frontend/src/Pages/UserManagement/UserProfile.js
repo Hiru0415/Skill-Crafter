161c61d3e8046ab8e461d24beaeb3d357291b116
Hiruni Weerasinghe <hiruvishwani15@gmail.com>
2025-05-11 00:38:26 +0530

    Update Form

--- frontend/src/Pages/UserManagement/UserProfile.js
+++ frontend/src/Pages/UserManagement/UserProfile.js
@@ -77,17 +77,7 @@ function UserProfile() {
                                     <FaTools className='user_data_card_icon' /> {userData.skills.join(', ')}
                                 </p>
                             </div>
-                            <div className="profile-actions">
-                                <button 
-                                    onClick={() => navigate(`/updateUserProfile/${userData.id}`)} 
-                                    className="update-button"
-                                >
-                                    Update Profile
-                                </button>
-                                <button onClick={handleDelete} className="delete-button">
-                                    Delete Account
-                                </button>
-                            </div>
+                            
                         </div>
                     </div>
                 )}
