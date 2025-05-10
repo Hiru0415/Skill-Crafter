9dd2c253068f8b19729edf9c68491e9008404b4e
Hiruni Weerasinghe <hiruvishwani15@gmail.com>
2025-05-11 01:33:20 +0530

    Delete form group

--- frontend/src/Pages/AchievementsManagement/AddAchievements.js
+++ frontend/src/Pages/AchievementsManagement/AddAchievements.js
@@ -94,18 +94,7 @@ function AddAchievements() {
               </div>
             </div>
 
-            <div className="form-group">
-              <label>Title</label>
-              <input
-                type="text"
-                name="title"
-                className="form-input"
-                placeholder="Enter achievement title"
-                value={formData.title}
-                onChange={handleChange}
-                required
-              />
-            </div>
+            
 
             <div className="form-group">
               <label>Description</label>
